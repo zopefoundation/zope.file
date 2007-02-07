b56a2785c92bc4d63f347725f8279055f752603f
Alexander Heavner <alex@zope.com>
2007-02-07 21:51:42 +0000

    A setup.py file was added so that the package can be 'eggified' The __init__.py file was updated to make zope a namespace package

--- src/zope/__init__.py
+++ src/zope/__init__.py
@@ -1,1 +1,7 @@
-# This directory is a Python package.
+# this is a namespace package
+try:
+    import pkg_resources
+    pkg_resources.declare_namespace(__name__)
+except ImportError:
+    import pkgutil
+    __path__ = pkgutil.extend_path(__path__, __name__)
