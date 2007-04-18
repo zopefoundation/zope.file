1c84e218f09263f43ec41bae4347c769d716f4f0
Gary Poster <gary@modernsongs.com>
2007-04-18 03:43:27 +0000

    make it possible to run tests.  Note that this reveals that the ftests are currently wildly broken in the trunk (which has the blob support).  A central problem is that the ftests use DemoStorage, and DemoStorage does not support blobs.

--- setup.py
+++ setup.py
@@ -2,11 +2,27 @@ from setuptools import setup, find_packages
 
 setup(
     name="zope.file",
-    version="0.1dev",
+    version="0.2dev",
     packages=find_packages('src'),
     package_dir={'':'src'},
     namespace_packages=['zope'],
     include_package_data=True,
-    install_requirements = ['setuptools'],
-    zip_safe = False
+    zip_safe = False,
+    install_requires=['setuptools',
+                      'zope.app.appsetup',
+                      'zope.app.publication',
+                      'zope.app.wsgi',
+                      'zope.event',
+                      'zope.interface',
+                      'zope.publisher',
+                      'zope.security',
+                      'zope.mimetype',
+                      # "extras"
+                      'zope.app.testing',
+                      'zope.app.securitypolicy',
+                      'zope.app.zcmlfiles',
+                      'zope.testbrowser',
+                      'zope.formlib',
+                      'zope.app.server',
+                      ],
     )
