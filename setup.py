8a25f8e9525448cdd1d98432d8bb06f6d932ca5c
Christian Zagrodnick <cz@gocept.com>
2008-03-11 09:55:41 +0000

    version to 0.4.0dev

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.file',
-      version = '0.4.0',
+      version = '0.4.0dev',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       description='Efficient File Implementation for Zope Applications',
