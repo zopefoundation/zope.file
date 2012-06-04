b62d5b2e1d59a597e31b29ffd8cb492f702c72a4
Uli Fouquet <uli@gnufix.de>
2012-06-04 15:54:22 +0000

    Back to work: 0.6.3dev.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.file',
-      version='0.6.2dev',
+      version='0.6.3dev',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Efficient File Implementation for Zope Applications',
